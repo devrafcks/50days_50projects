c52d602f1560bf7cfff248d36e7814965efed80c
Rafcksdev <rafcksps@gmail.com>
2025-06-08 01:17:59 -0300

    added day 23 project

---
+++ Day 23 - Kinect loader/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const loader = document.querySelector('.kinetic');
+  
+  setTimeout(() => {
+    loader.style.display = 'none';
+  }, 3500); 
+});
